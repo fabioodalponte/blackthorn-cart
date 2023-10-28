a3cf6755aa0e87167a4ec6a1188b1309af43bcf6
fabioodalponte <36447864+fabioodalponte@users.noreply.github.com>
2023-10-28 09:32:05 -0300

    finishing item controller

--- blackthron-cart-backend/src/items/items.controller.ts
+++ blackthron-cart-backend/src/items/items.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Item } from './entity/item.entity';
+import { ItemsService } from './items.service';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    constructor(private itemsService: ItemsService) {}
+
+    @Post()
+    create(@Body() itemData: Partial<Item>) {
+        return this.itemsService.createItem(itemData);
+    }
+
+    @Get()
+    findAll() {
+        return this.itemsService.findAllItems();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:number) {
+        return this.itemsService.findItemById(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id:number, @Body() itemData: Partial<Item>) {
+        return this.itemsService.updateItem(id, itemData);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:number)  {
+        return this.itemsService.deleteItem(id);
+    }
+
+}
--- blackthron-cart-backend/src/items/items.service.ts
+++ blackthron-cart-backend/src/items/items.service.ts
@@ -23,5 +23,15 @@ export class ItemsService {
     async findItemById(id: number): Promise<Item | undefined> {
         return this.itemRepository.findOneBy({id});
     }
-    
+
+    async updateItem(id:number, item: Partial<Item>): Promise<Item> {
+        await this.itemRepository.update(id, item);
+        return this.findItemById(id);
+    }
+
+    async deleteItem(id: number): Promise<boolean> {
+        const deteleItem = await this.itemRepository.delete(id);
+        return deteleItem.affected > 0;
+    }
+
 }
